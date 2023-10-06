f87bc59cb06125a9b63ae14cf2b4ef0627fd3e64
Dmitry Golubev <dmitry.golubev@vk.team>
2023-10-06 15:33:12 +0300

    добавил схему

---
+++ POSNetworking/POSNetworking.h
@@ -0,0 +1,42 @@
+//
+//  POSNetworking.h
+//  POSNetworking
+//
+//  Created by dmitry.golubev on 06.10.2023.
+//  Copyright © 2023 Pavel Osipov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <POSNetworking/POSHTTPGateway.h>
+#import <POSNetworking/POSHTTPGatewayOptions.h>
+
+#import <POSNetworking/POSStaticHost.h>
+
+#import <POSNetworking/POSHTTPGET.h>
+#import <POSNetworking/POSHTTPHEAD.h>
+#import <POSNetworking/POSHTTPPOST.h>
+#import <POSNetworking/POSHTTPPUT.h>
+
+#import <POSNetworking/POSHTTPRequest.h>
+#import <POSNetworking/POSHTTPRequestOptions.h>
+
+#import <POSNetworking/POSHTTPResponse.h>
+#import <POSNetworking/POSHTTPResponseOptions.h>
+
+#import <POSNetworking/NSDictionary+POSNetworking.h>
+#import <POSNetworking/NSError+POSNetworking.h>
+#import <POSNetworking/NSString+POSNetworking.h>
+#import <POSNetworking/NSURL+POSNetworking.h>
+#import <POSNetworking/NSNumber+POSNetworking.h>
+#import <POSNetworking/NSHTTPURLResponse+POSNetworking.h>
+
+
+//! Project version number for POSNetworking.
+FOUNDATION_EXPORT double POSNetworkingVersionNumber;
+
+//! Project version string for POSNetworking.
+FOUNDATION_EXPORT const unsigned char POSNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POSNetworking/PublicHeader.h>
+
+
